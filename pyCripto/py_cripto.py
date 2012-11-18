12bdf3ebc5601a4b00713b462f95eaee04f06f5b
Helam <helam.dev@gmail.com>
2012-11-18 15:12:27 -0200

    UPDATE
    Special charac verify
    Number  verify

--- pyCripto/py_cripto.py
+++ pyCripto/py_cripto.py
@@ -7,47 +7,96 @@ from Tkinter import *
 class Criptografy:
 	def __init__(self):
 		self.alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
-		self.special = [ [["Á", "À", "Ã", "Â"], "A"], [["É", "È", "Ê"], "E"], [["Í", "Ì"], "I"], [["Ó", "Ò", "Ô", "Õ"], "O"], [["Ú", "Ù", "Ü"], "U"] ]
+		self.number = ['0','1','2','3','4','5','6','7','8','9']
+		self.special = [ [["Á", "À", "Ã", "Â"], "A"], [["É", "È", "Ê"], "E"], [["Í", "Ì"], "I"], [["Ó", "Ò", "Ô", "Õ"], "O"], [["Ú", "Ù", "Ü"], "U"], [["Ç"], "C"] ]
 		self.key = 0
-		print len(self.alphabet)
 
+	""" Verifica os letras com caracteres especiais e subustitui-las"""
+	def SpecialCharReplace(self, word=''):		
+		word = word.upper()
+		word = word.encode(encoding='UTF-8', errors='strict')			
+		for i, xlist in enumerate(self.special):
+			for yindice, ylist in enumerate(xlist):
+				for zlist in ylist:
+					if zlist == word:							
+						print(zlist)
+						print(xlist[1])
+						return xlist[1]
+		return 	word			
+
+
+	""" Função para criptografar e decriptografar o texto"""
 	def cript(self,action, text, key):
 		self.key = key
 		text = text.upper()
+		text = str(text)
 		newText = ''
 
+		""" Loop para percorrer o texto original"""
 		for wordText in text:
+			""" Chama função para subustituir os caracteres especiais"""
+			wordText = self.SpecialCharReplace(wordText)		
+			
+			""" contator para pegar o key das letras"""
 			count = 0
+			
+			""" Contador para pegar o key dos numeros"""
+			countN = 0
+			
+			""" Loop para percorrer a lista do alfabeto"""
 			for wordAlpha in self.alphabet:
-				if wordText == wordAlpha:				
 
+				""" Compara a letra do texto com a do alfabeto"""
+				if wordText == wordAlpha:
+					""" Verifica a ação. Se for 'cript' então codifica a letra """
 					if action == 'cript':
+
+						""" soma o valor da chave com o indice da letra """
 						soma = count+self.key
+						""" Se a soma for maior q 25 subtrai o total de letras, para voltar ao key 0 """
 						if soma > 25:
 							countSoma = soma - 26
 							newText += self.alphabet[0+countSoma]
 						else:
 							newText += self.alphabet[soma]
+
+					""" Verifica a ação. Se for 'decript' então decodifica a letra """
 					if action == 'decript':
 						soma = count-self.key
 						if soma < 0:
 							soma = 26
 						newText += self.alphabet[soma]
+				count += 1
 
-					print wordText, count, newText	
-				count += 1	
 
-				
+			""" Verifica os numeros """
+			for number in self.number:
+				if wordText == number:
+					if action == 'cript':
+						soma = countN+self.key
+						if soma > 9:
+							countSoma = soma - 9
+							newText += self.number[0+countSoma]
+						else:
+							newText += self.number[soma]
+					if action == 'decript':
+						soma = countN-self.key
+						if soma < 0:
+							soma = 10
+						newText += self.number[soma]
+				countN += 1
 
 			if wordText == ' ':
 				newText += ' '
-				print wordText, count, newText
 
-		print newText
 		return newText
 
 	def decript(self):
 		pass	
+
+
+
+
 
 
 
@@ -76,7 +125,7 @@ class PyCriptoApp:
 
 		self.textInputFrame = Frame(self.textFrame)
 		self.textInputField = Text(self.textInputFrame, height='20', width='40')
-		self.textInputLabel = Label(self.textInputFrame, text='Texto Origonal: ')
+		self.textInputLabel = Label(self.textInputFrame, text='Texto origonal: ')
 		self.criptButton	= Button(self.textInputFrame, text='Criptografar', font='Arial 12', command=self.cript)
 
 		""" Define a caixa de texto Resposta"""
@@ -106,10 +155,11 @@ class PyCriptoApp:
 		self.decriptButton.pack(side='bottom')
 
 
+
 	def cript(self):
 		key = int(self.keyField.get())
 
-		criptoObj = Criptografy()		
+		criptoObj = Criptografy()
 
 		originalText = self.textInputField.get('1.0', 'end')
 
@@ -117,9 +167,6 @@ class PyCriptoApp:
 
 		self.textResponseField.delete('1.0', 'end')
 		self.textResponseField.insert(END, criptedText)
-
-		print originalText
-		print key
 
 
 	def decript(self):
@@ -134,9 +181,8 @@ class PyCriptoApp:
 		self.textInputField.delete('1.0', 'end')
 		self.textInputField.insert(END, decriptedText)
 
-		print originalText
-		print key
 
 
 
-app = PyCriptoApp('800x600')
+
+app = PyCriptoApp('800x500')
